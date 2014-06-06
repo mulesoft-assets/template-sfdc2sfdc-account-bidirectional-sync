34d4abab0d2b4c1230c1e6e41fec9e0c17c359f3
Marek <marecica2@gmail.com>
2014-06-06 16:11:22 +0200

    renamed tests

--- src/test/java/org/mule/templates/integration/BidirectionalAccountSyncIT.java
+++ src/test/java/org/mule/templates/integration/BidirectionalAccountSyncIT.java
@@ -3,7 +3,6 @@ package org.mule.templates.integration;
 import static org.mule.templates.builders.SfdcObjectBuilder.anAccount;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -16,13 +15,9 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.mule.DefaultMuleMessage;
 import org.mule.MessageExchangePattern;
-import org.mule.api.MuleEvent;
 import org.mule.api.MuleException;
-import org.mule.api.MuleMessage;
 import org.mule.api.lifecycle.InitialisationException;
-import org.mule.construct.Flow;
 import org.mule.processor.chain.InterceptingChainLifecycleWrapper;
 import org.mule.processor.chain.SubflowInterceptingChainLifecycleWrapper;
 import org.mule.templates.AbstractTemplatesTestCase;
@@ -39,7 +34,7 @@ import com.sforce.soap.partner.SaveResult;
  * 
  */
 @SuppressWarnings("unchecked")
-public class BidirectionalAccountPushNotificationTest extends AbstractTemplatesTestCase {
+public class BidirectionalAccountSyncIT extends AbstractTemplatesTestCase {
 
 	private static final String ANYPOINT_TEMPLATE_NAME = "sfdc2sfdc-bidirectional-account-sync";
 	private static final String A_INBOUND_FLOW_NAME = "triggerSyncFromAFlow";
@@ -51,7 +46,7 @@ public class BidirectionalAccountPushNotificationTest extends AbstractTemplatesT
 	private static SubflowInterceptingChainLifecycleWrapper deleteAccountFromAFlow;
 	private static SubflowInterceptingChainLifecycleWrapper deleteAccountFromBFlow;
 	
-	private Flow triggerPushFlow;
+
 	private SubflowInterceptingChainLifecycleWrapper createAccountInAFlow;
 	private SubflowInterceptingChainLifecycleWrapper createAccountInBFlow;
 	private InterceptingChainLifecycleWrapper queryAccountFromAFlow;
@@ -92,10 +87,8 @@ public class BidirectionalAccountPushNotificationTest extends AbstractTemplatesT
 		stopFlowSchedulers(B_INBOUND_FLOW_NAME);
 	}
 
-  	private void getAndInitializeFlows() throws InitialisationException {
+	private void getAndInitializeFlows() throws InitialisationException {
 		// Flow for creating accounts in sfdc A instance
-		triggerPushFlow = getFlow("triggerPushFlow");
-
 		createAccountInAFlow = getSubFlow("createAccountInAFlow");
 		createAccountInAFlow.initialise();
 
@@ -140,28 +133,40 @@ public class BidirectionalAccountPushNotificationTest extends AbstractTemplatesT
 	public void whenUpdatingAnAccountInInstanceBTheBelongingAccountGetsUpdatedInInstanceA()
 			throws MuleException, Exception {
 
+		// Build test accounts
+		SfdcObjectBuilder account = anAccount()
+				.with("Name", ANYPOINT_TEMPLATE_NAME + "-" + System.currentTimeMillis() + "Account")
+				.with("Phone", "123456789");
+
+		SfdcObjectBuilder justCreatedAccount = account.with("Description",
+				"Old description");
+		SfdcObjectBuilder updatedAccount = account.with("Description",
+				"Some nice description");
+
+		// Create accounts in sand-boxes and keep track of them for posterior
+		// cleaning up
+		accountsCreatedInA.add(createTestAccountsInSfdcSandbox(
+				justCreatedAccount.build(), createAccountInAFlow));
+		accountsCreatedInB.add(createTestAccountsInSfdcSandbox(
+				updatedAccount.build(), createAccountInBFlow));
+
 		// Execution
-		String accountName = buildUniqueName();
-		MuleMessage message = new DefaultMuleMessage(buildRequest(accountName), muleContext);
-		MuleEvent testEvent = getTestEvent(message, MessageExchangePattern.REQUEST_RESPONSE);
-		testEvent.setFlowVariable("sourceSystem", "A");
-		triggerPushFlow.process(testEvent);
-		
-		batchTestHelper.awaitJobTermination(TIMEOUT_MILLIS * 1000, 500);
-		batchTestHelper.assertJobWasSuccessful();
+		executeWaitAndAssertBatchJob(B_INBOUND_FLOW_NAME);
 
 		// Assertions
-		HashMap<String, Object> account = new HashMap<String, Object>();
-		account.put("Name", accountName);
+		Map<String, String> retrievedAccountFromA = (Map<String, String>) queryAccount(
+				account.build(), queryAccountFromAFlow);
 		Map<String, String> retrievedAccountFromB = (Map<String, String>) queryAccount(
-				account, queryAccountFromBFlow);
-		System.err.println(retrievedAccountFromB);
-
-		Assert.assertNotNull(retrievedAccountFromB);
-		Assert.assertEquals("Account Names should be equals", account.get("Name"), retrievedAccountFromB.get("Name"));
+				account.build(), queryAccountFromBFlow);
 		
-		// delete previously created account in B
-		accountsCreatedInB.add(retrievedAccountFromB.get("Id"));
+		retrievedAccountFromA.remove("Id");
+		retrievedAccountFromB.remove("Id");
+		
+		final MapDifference<String, String> differences = Maps.difference(
+				retrievedAccountFromA, retrievedAccountFromB);
+		Assert.assertTrue(
+				"Some accounts are not synchronized between systems. "
+						+ differences.toString(), differences.areEqual());
 	}
 
 	private Object queryAccount(Map<String, Object> account,
@@ -174,66 +179,29 @@ public class BidirectionalAccountPushNotificationTest extends AbstractTemplatesT
 				.getMessage().getPayload();
 	}
 
+	private String createTestAccountsInSfdcSandbox(Map<String, Object> account,
+			InterceptingChainLifecycleWrapper createAccountFlow)
+			throws MuleException, Exception {
+		List<Map<String, Object>> salesforceAccounts = new ArrayList<Map<String, Object>>();
+		salesforceAccounts.add(account);
 
-	private String buildRequest(String accountName){
-		String req = "";
-		req += "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
-		req += "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">";
-		req += " <soapenv:Body>";
-		req += "  <notifications xmlns=\"http://soap.sforce.com/2005/09/outbound\">";
-		req += "   <OrganizationId>00Dd0000000dtDqEAI</OrganizationId>";
-		req += "   <ActionId>04kd0000000PCgvAAG</ActionId>";
-		req += "   <SessionId xsi:nil=\"true\"/>";
-		req += "   <EnterpriseUrl>https://na14.salesforce.com/services/Soap/c/30.0/00Dd0000000dtDq</EnterpriseUrl>";
-		req += "   <PartnerUrl>https://na14.salesforce.com/services/Soap/u/30.0/00Dd0000000dtDq</PartnerUrl>";
-		req += "   <Notification>";
-		req += "    <Id>04ld000000TzMKpAAN</Id>";
-		req += "    <sObject xsi:type=\"sf:Account\" xmlns:sf=\"urn:sobject.enterprise.soap.sforce.com\">";
-		req += "     <sf:Id>001d000001XD5XKAA1</sf:Id>";
-		req += "     <sf:AccountNumber>4564564</sf:AccountNumber>";
-		req += "     <sf:AnnualRevenue>10000.0</sf:AnnualRevenue>";
-		req += "     <sf:BillingCity>City</sf:BillingCity>";
-		req += "     <sf:BillingCountry>Country</sf:BillingCountry>";
-		req += "     <sf:BillingPostalCode>04001</sf:BillingPostalCode>";
-		req += "     <sf:BillingState>State</sf:BillingState>";
-		req += "     <sf:BillingStreet>Street</sf:BillingStreet>";
-		req += "     <sf:CreatedById>005d0000000yYC7AAM</sf:CreatedById>";
-		req += "     <sf:CreatedDate>2014-05-05T11:47:49.000Z</sf:CreatedDate>";
-		req += "     <sf:CustomerPriority__c>High</sf:CustomerPriority__c>";
-		req += "     <sf:Description>description ddddd</sf:Description>";
-		req += "     <sf:Fax>+421995555</sf:Fax>";
-		req += "     <sf:Industry>Apparel</sf:Industry>";
-		req += "     <sf:IsDeleted>false</sf:IsDeleted>";
-		req += "     <sf:LastModifiedById>005d0000000yYC7AAM</sf:LastModifiedById>";
-		req += "     <sf:LastModifiedDate>2014-06-02T13:00:00.000Z</sf:LastModifiedDate>";
-		req += "     <sf:LastReferencedDate>2014-05-19T11:02:14.000Z</sf:LastReferencedDate>";
-		req += "     <sf:LastViewedDate>2014-05-19T11:02:14.000Z</sf:LastViewedDate>";
-		req += "     <sf:Name>"+accountName+"</sf:Name>";
-		req += "     <sf:NumberOfEmployees>5000</sf:NumberOfEmployees>";
-		req += "     <sf:OwnerId>005d0000000yYC7AAM</sf:OwnerId>";
-		req += "     <sf:Ownership>Public</sf:Ownership>";
-		req += "     <sf:Phone>+421995555</sf:Phone>";
-		req += "     <sf:PhotoUrl>/services/images/photo/001d000001XD5XKAA1</sf:PhotoUrl>";
-		req += "     <sf:Rating>Hot</sf:Rating>";
-		req += "     <sf:SLA__c>Gold</sf:SLA__c>";
-		req += "     <sf:ShippingCity>Shipping City</sf:ShippingCity>";
-		req += "     <sf:ShippingCountry>Country</sf:ShippingCountry>";
-		req += "     <sf:ShippingPostalCode>04001</sf:ShippingPostalCode>";
-		req += "     <sf:ShippingState>Shipping State</sf:ShippingState>";
-		req += "     <sf:ShippingStreet>Shipping street</sf:ShippingStreet>";
-		req += "     <sf:Site>http://www.test.com</sf:Site>";
-		req += "     <sf:SystemModstamp>2014-05-19T11:02:14.000Z</sf:SystemModstamp>";
-		req += "     <sf:Type>Prospect</sf:Type>";
-		req += "     <sf:Website>http://www.test.com</sf:Website>";
-		req += "    </sObject>";
-		req += "   </Notification>";
-		req += "  </notifications>";
-		req += " </soapenv:Body>";
-		req += "</soapenv:Envelope>";
-		return req;
+		final List<SaveResult> payloadAfterExecution = (List<SaveResult>) createAccountFlow
+				.process(
+						getTestEvent(salesforceAccounts,
+								MessageExchangePattern.REQUEST_RESPONSE))
+				.getMessage().getPayload();
+		return payloadAfterExecution.get(0).getId();
 	}
-	
-	private String buildUniqueName() {
-		return ANYPOINT_TEMPLATE_NAME + "-" + System.currentTimeMillis() + "Account";
+
+	private void executeWaitAndAssertBatchJob(String flowConstructName)
+			throws Exception {
+
+		// Execute synchronization
+		runSchedulersOnce(flowConstructName);
+
+		// Wait for the batch job execution to finish
+		batchTestHelper.awaitJobTermination(TIMEOUT_MILLIS * 1000, 500);
+		batchTestHelper.assertJobWasSuccessful();
 	}
+
 }
